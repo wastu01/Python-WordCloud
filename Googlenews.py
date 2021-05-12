4f91814317e58a88911506c0d3e0e90a0111377a
wa.code <47618277+wastu01@users.noreply.github.com>
2021-05-13 02:55:47 +0800

    刪去程式多餘空白及註解
    順便更新文字雲圖片

--- Googlenews.py
+++ Googlenews.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[51]:
-
-
-# https://github.com/youngmihuang/lyrics_application
-# https://pypi.org/project/GoogleNews/
-
-from GoogleNews import GoogleNews
-
-import requests 
-from bs4 import BeautifulSoup
 import pandas as pd
+import requests
+import jieba
+from GoogleNews import GoogleNews
+from bs4 import BeautifulSoup
 
 googlenews = GoogleNews()
 
@@ -19,7 +12,6 @@ googlenews.setlang('cn')
 googlenews.setperiod('d')
 googlenews.setencode('utf-8')
 googlenews.clear()
-
 
 x = input("請輸入要搜尋的關鍵字，將為你搜集相關字詞內容:")
 googlenews.search(x)
@@ -35,39 +27,27 @@ links = googlenews.get_links()
 print()
 
 for n in range(len(result)):
-#     print(n+1)
     print(result[n])
     print(links[n])
-   
 
 df = pd.DataFrame(
-{
-    '標題': result,
-    '連結': links
-})
-
-
-
-# In[52]:
-
-
+    {
+        '標題': result,
+        '連結': links
+    })
 
 url = df['連結'][0]
 print(url)
 
-
-user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
-
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
 
 r = requests.get(url, headers=user_agent)
 r.encoding = "utf-8"
 web_content = r.text
-soup = BeautifulSoup(web_content,'html')
-
+soup = BeautifulSoup(web_content, 'html', 'features="html.parser"')
 
 articleContent = soup.find_all('p')
-articleContent
-
 
 # In[53]:
 
@@ -76,54 +56,36 @@ article = []
 for p in articleContent:
     article.append(p.text)
 
-
-# In[54]:
-
-
-
 articleAll = '\n'.join(article)
-print(articleAll)
+# print(articleAll)
 
 # 分段用
 
 
-# In[55]:
-
-
-
-import jieba
 jieba.load_userdict('dict.txt.big.txt')
 
-d = articleAll.replace('[^\w\s]','').replace('／',"").replace('《','').replace('》','').replace('，','').replace('。','').replace('「','').replace('」','').replace('（','').replace('）','').replace('！','').replace('？','').replace('、','').replace('▲','').replace('…','').replace('：','')
+d = articleAll.replace('[^\w\s]', '').replace('／', "").replace('《', '').replace('》', '').replace('，', '').replace('。',
+                                                                                                                  '').replace(
+    '「', '').replace('」', '').replace('（', '').replace('）', '').replace('！', '').replace('？', '').replace('、',
+                                                                                                          '').replace(
+    '▲', '').replace('…', '').replace('：', '')
 # print(d)
 
 jieba.setLogLevel(20)
 
 # Sentence = jieba.cut(d, cut_all=True)
 # print('全模式'+": "  + "/ ".join(Sentence) + '\n')   
- 
+
 # Sentence = jieba.cut(d, cut_all=False)
 # print('精確模式'+": " + "/ ".join(Sentence)+ '\n')  
- 
+
 # Sentence = jieba.cut(d)  
 # print('Default為精確模式'+": " + "/ ".join(Sentence)+ '\n')
- 
-Sentence = jieba.cut_for_search(d)  
-print('搜索引擎模式'+": " + "/ ".join(Sentence)+ '\n')
 
+Sentence = jieba.cut_for_search(d)
+print('搜索引擎模式' + ": " + "/ ".join(Sentence) + '\n')
 
-# In[56]:
-
-
-
-artDf = pd.DataFrame.from_dict(terms, orient='index', columns = ['詞頻'])
-artDf.sort_values(by= ['詞頻'], ascending= False)
-
-
-# In[57]:
-
-
-
+Sentence = jieba.cut_for_search(d)
 
 import numpy as np
 
@@ -131,101 +93,61 @@ from PIL import Image
 
 import matplotlib.pyplot as plt
 
-from collections import Counter
-
 from wordcloud import WordCloud, ImageColorGenerator
 
 from scipy.ndimage import gaussian_gradient_magnitude
 
-
-
-with open('stopword.txt','r',encoding="utf-8") as f:
-    stops = f.read().split('\n')
-    
-
-Sentence = jieba.cut_for_search(d)    
-
+with open('stopword.txt', 'r', encoding="utf-8") as f:
+    stopwords = f.read().split('\n')
 
 terms = {}
-for sentence in Sentence :
-    if sentence in stops:
+for sentence in Sentence:
+    if sentence in stopwords:
         continue
-    
+
     if sentence in terms:
         terms[sentence] += 1
     else:
-        terms[sentence] = 1  
-        
-        
-dicition = Counter(terms)
+        terms[sentence] = 1
+
 # print(Counter(terms))
-
-
-
-
-
-        
 # generate_from_text()方法會採納stopwords參數 也可使用
 
 # https://coolors.co/palettes/popular
 
 
+artDf = pd.DataFrame.from_dict(terms, orient='index', columns=['詞頻'])
+artDf.sort_values(by=['詞頻'], ascending=False)
 
+img = "color"
+img_path = "%s.png" % img
 
-
-icon = "colortest01"
-icon_path = "%s.jpg" % icon
-
-
-mask_color = np.array(Image.open(icon_path))
+mask_color = np.array(Image.open(img_path))
 mask_color = mask_color[::3, ::3]
 mask_image = mask_color.copy()
 mask_image[mask_image.sum(axis=2) == 0] = 255
 
-
-edges = np.mean([gaussian_gradient_magnitude(mask_color[:, :, i]/255., 2) for i in range(3)], axis=0)
+edges = np.mean([gaussian_gradient_magnitude(mask_color[:, :, i] / 255., 2) for i in range(3)], axis=0)
 mask_image[edges > .08] = 255
 
-
-
-
-
-
-wc = WordCloud(font_path="/Users/larry/Library/Fonts/SourceHanSansTW-Regular.otf", 
-               mask = mask_color,
-               max_font_size=45, 
-               max_words = 2000 ,        
-               stopwords= stops,
-                margin=1,
-              relative_scaling=0)     
+wc = WordCloud(font_path="/Users/larry/Library/Fonts/SourceHanSansTC-Bold.otf",
+               mask=mask_color,
+               max_font_size=45,
+               max_words=2000,
+               stopwords=stopwords,
+               margin=1,
+               relative_scaling=0)
 
 wc.generate_from_frequencies(terms)
- 
-
-
 image_colors = ImageColorGenerator(mask_color)
 wc.recolor(color_func=image_colors)
 
-
 # 視覺化
 
-plt.imshow(wc , interpolation="bilinear")
+plt.imshow(wc, interpolation="bilinear")
 plt.axis("off")
-plt.figure(figsize=(25,25))
+plt.figure(figsize=(25, 25))
 plt.show()
 
 # plt.savefig("Wordcloud.png")
-wc.to_file("demo04.png")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+wc.to_file("img/demo08.png")
