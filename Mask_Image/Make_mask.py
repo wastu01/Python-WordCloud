108e864439f17888c356b3bc457ba0aca362738f
wastu01 <adt107118@gm.ntcu.edu.tw>
2024-09-03 23:06:34 +0800

    Create Make_mask.py

---
+++ Mask_Image/Make_mask.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw
+import ast
+
+# 設定圖像大小
+image_size = 512  # 圖像的寬度和高度
+small_square_size = 30  # 每個小正方形的寬度和高度
+
+# 讀取 color_list.txt 文件中的顏色
+with open('color_list.txt', 'r') as f:
+    # 讀取文件內容並將其轉換為列表
+    colors = f.readline().strip().split(",")
+    print(type(colors))
+
+# 創建空白圖像
+img = Image.new("RGB", (image_size, image_size), "white")
+draw = ImageDraw.Draw(img)
+
+# 畫小正方形並填充顏色
+color_index = 0
+for i in range(0, image_size, small_square_size):
+    for j in range(0, image_size, small_square_size):
+        # 選擇顏色
+        color = colors[color_index % len(colors)]
+        # 畫小正方形並填充選定的顏色
+        draw.rectangle([i, j, i + small_square_size, j + small_square_size], fill=color)
+        # 更新顏色索引
+        color_index += 1
+
+# # 儲存圖片
+img.show()
+img.save("./img/square_mask1.png")
