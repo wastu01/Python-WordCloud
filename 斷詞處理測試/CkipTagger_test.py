10288a8056376232d118e5f857161f4d1bc26b4f
wastu01 <adt107118@gm.ntcu.edu.tw>
2024-09-21 13:33:53 +0800

    Update CkipTagger_test.py

--- 斷詞處理測試/CkipTagger_test.py
+++ 斷詞處理測試/CkipTagger_test.py
@@ -1,8 +1,16 @@
+# official repo readme
+# https://github.com/ckiplab/ckiptagger
+
+
 # https://colab.research.google.com/drive/1kaO0LbsOJVEBTee23UDjJxxP9emxBrB0
 # https://tako-analytics.com/2023-06-19-how-to-extract-keywords-from-traditional-chinese-articles-in-nlp/
 # only for 斷詞
 
-# from this tutorial
+# tutorial
 # https://blog.infuseai.io/nlp-tool-ckiptagger-introduction-e34637184e67
 
 # 待研究
+
+# 另外創一個虛擬環境
+# Mac M1 安裝 ckiptagger 有 tensorflow 版本問題 
+# 改裝 ckip-transformers
